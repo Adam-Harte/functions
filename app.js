7a5e805fda3aed05a884c3f0c284bd5c03ae1c25
Adam-Harte <adamharte11@hotmail.co.uk>
2020-03-08 00:35:41 +0000

    function expression added

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 //function declaration
-function sayHello {
-   console.log('Hello');
+function sayHello() {
+  console.log('Hello');
 }
 
 sayHello();
 console.log(typeof sayHello);
 console.dir(sayHello);
+
+//function expression
+const sayBye = function() {
+  console.log('bye');
+};
+
+sayBye();
